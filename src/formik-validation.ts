1170f2ae85beae0d22a9f9fa018df2478624c7c5
Braulio Diez Botella <braulio.diez@gmail.com>
2019-10-17 20:11:42 +0200

    pending discussion

--- src/formik-validation.ts
+++ src/formik-validation.ts
@@ -18,23 +18,43 @@ export class FormikValidation {
     this.formValidation = createFormValidation(validationSchema);
   }
 
+  // Discuss here, Promise<any> return value is not really used
   public validateField(
     fieldId: string,
     value: any,
     values?: any
   ): Promise<ValidationResult> {
-    // TODO
-    return null;
+    return this.formValidation.validateField(name, value).then(result => {
+      if (!result.succeeded) {
+        throw result;
+      }
+
+      return null;
+    });
   }
 
   public validateRecord(values: any): Promise<RecordValidationResult> {
-    // TODO
-    return null;
+    return this.formValidation.validateRecord(values).then(result => {
+      if (!result.succeeded) {
+        throw result;
+      }
+
+      return null;
+    });
   }
 
   public validateForm(values: any): Promise<FormValidationResult> {
-    // TODO
-    return null;
+    return this.formValidation.validateForm(values).then(result => {
+      if (!result.succeeded) {
+        // To be discussed here should we return only FielValidation
+        // results? if not it won't be able to match field and
+        // validations
+        // throw result.fieldErrors
+        throw result;
+      }
+
+      return null;
+    });
   }
 }
 
